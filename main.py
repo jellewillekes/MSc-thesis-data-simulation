4e57dbafa904073f1101df07f58f1e2476d1f289
jwillekesmct <jwillekes18@gmail.com>
2023-04-22 22:01:48 +0200

    Update for data simulation

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import uniform, norm, expon, gamma, beta, binom, poisson, lognorm, geom, weibull_min
-import string
-from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
-
-import matplotlib.pyplot as plt
+from scipy.stats import uniform, norm, expon, lognorm, gamma, beta, weibull_min, pareto, cauchy, chi2, bernoulli, binom, \
+    geom, hypergeom, nbinom, poisson, randint, zipf, logser, dlaplace
+from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
 
 np.random.seed(99)  # For reproducibility
 n_samples = 100000
 n_cat = 20
 n_num = 20
 
-# Generate data for each distribution
-uniform_data = uniform.rvs(loc=0, scale=10, size=(n_samples, 2))
-normal_data = norm.rvs(loc=0, scale=1, size=(n_samples, 2))
-exponential_data = expon.rvs(scale=1, size=(n_samples, 2))
-gamma_data = gamma.rvs(a=2, scale=1, size=(n_samples, 2))
-beta_data = beta.rvs(a=2, b=5, size=(n_samples, 2))
-binomial_data = binom.rvs(n=10, p=0.5, size=(n_samples, 2))
-poisson_data = poisson.rvs(mu=3, size=(n_samples, 2))
-lognormal_data = lognorm.rvs(s=0.5, scale=np.exp(0.5), size=(n_samples, 2))
-geometric_data = geom.rvs(p=0.3, size=(n_samples, 2))
-weibull_data = weibull_min.rvs(c=1.5, scale=2, size=(n_samples, 2))
+# Continuous Distributions
+normal_data = norm.rvs(loc=0, scale=1, size=(n_samples, 4))
+exponential_data = expon.rvs(scale=1, size=(n_samples, 4))
+lognormal_data = lognorm.rvs(s=1, size=(n_samples, 4))
+uniform_data = uniform.rvs(loc=0, scale=10, size=(n_samples, 4))
+weibull_data = weibull_min.rvs(c=2, scale=1, size=(n_samples, 4))
 
-# Combine data into a single dataset
+# Discrete Distributions
+discrete_distributions = [
+    bernoulli.rvs(p=0.5, size=(n_samples, 4)),
+    binom.rvs(n=10, p=0.5, size=(n_samples, 4)),
+    geom.rvs(p=0.5, size=(n_samples, 4)),
+    poisson.rvs(mu=3, size=(n_samples, 4)),
+    dlaplace.rvs(loc=0, a=1, size=(n_samples, 4))
+]
+
+# Shift and map discrete samples to alphabetical categories
+alphabet = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+categorical_data = []
+
+for dist_data in discrete_distributions:
+    shifted_data = dist_data - np.min(dist_data)
+    max_value = np.max(shifted_data)
+    categorical_columns = np.array([alphabet[i % len(alphabet)] for i in range(max_value + 1)])
+    categorical_data.append(categorical_columns[shifted_data])
+
+# Combine numerical and categorical data
 data = np.hstack(
-    [uniform_data, normal_data, exponential_data, gamma_data, beta_data, binomial_data, poisson_data, lognormal_data,
-     geometric_data, weibull_data])
+    [normal_data, exponential_data, lognormal_data, uniform_data, weibull_data] + categorical_data)
 
 # Create a DataFrame
-columns = [f'F{i + 1}' for i in range(n_num)]
-df = pd.DataFrame(data, columns=columns)
+columns = [f'F{i + 1}' for i in range(n_num + n_cat)]
+dataset = pd.DataFrame(data, columns=columns)
 
-# Alphabet letters to use as labels
-alphabet = list(string.ascii_uppercase)[:10]
-
-# Add 25 categorical variables with random number of labels between 2 and 10
-for i in range(1, n_cat+1):
-    num_labels = np.random.randint(2, 11)
-    num_digits = np.random.randint(1, 4)
-    letter = np.random.choice(alphabet, num_labels, replace=False)
-    labels = [s.strip() * num_digits for s in letter]
-    df[f'F{i + n_num}'] = np.random.choice(labels, n_samples)
-
-dataset = df.copy()
+df = dataset.copy()
 
 # Convert categorical features to numerical values using ordinal encoding
 ordinal_encoder = OrdinalEncoder()
 category_columns = [f'F{i + n_cat}' for i in range(1, n_cat + 1)]
 df[category_columns] = ordinal_encoder.fit_transform(df[category_columns])
+
+# Convert numerical columns to float64
+numerical_columns = [f'F{i}' for i in range(1, n_num + 1)]
+df[numerical_columns] = df[numerical_columns].astype('float64')
 
 
 # Define a nonlinear transformation to generate the target variable Y
@@ -56,20 +60,19 @@ def nonlinear_transformation(row):
     result = 0
 
     # Numerical features
-    for i in range(1, n_num + 1, n_num//2):
+    for i in range(1, n_num + 1, n_num // 2):
         num_product_term = row[f'F{i}'] * row[f'F{(i % n_num) + 1}']
         num_square_term = row[f'F{i + 1}'] ** 2
         num_sqrt_term = np.sqrt(np.abs(row[f'F{i + 2}']))
         result += num_product_term + num_square_term + num_sqrt_term
 
     # Categorical features
-    for i in range(n_num + 1, n_num + n_cat, n_cat//2):
+    for i in range(n_num + 1, n_num + n_cat, n_cat // 2):
         cat_product_term = row[f'F{i}'] * row[f'F{(i % n_cat) + n_num + 1}']
         cat_difference_term = np.abs(row[f'F{i + 1}'] - row[f'F{(i % n_cat) + n_num + 2}'])
         result += cat_product_term + cat_difference_term
 
     return result
-
 
 # Apply the nonlinear transformation to obtain Y
 df['Y'] = df.apply(nonlinear_transformation, axis=1)
@@ -78,9 +81,8 @@ df['Y'] = df.apply(nonlinear_transformation, axis=1)
 scaler = MinMaxScaler(feature_range=(0, 100))
 df['Y'] = scaler.fit_transform(df['Y'].values.reshape(-1, 1))
 
-dataset['Y'] = df['Y']
-
 # Randomize the dataset
-dataset = dataset.sample(frac=1).reset_index(drop=True)
+df = df.sample(frac=1).reset_index(drop=True)
 
-dataset.to_csv('synthetic_dataset.csv', index=False)
+df.to_csv('synthetic_dataset_1.csv', index=False)
+
--- verification.py
+++ verification.py
@@ -1,59 +1,42 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-dataset = pd.read_csv('synthetic_dataset.csv')
+dataset = pd.read_csv('synthetic_dataset_1.csv')
 
 # Assuming 'dataset' is a pandas DataFrame with columns 'Y' and features
 target_col = 'Y'
-significance_level = 0.05
 
-# Separate numerical and categorical columns
-numerical_cols = dataset.select_dtypes(include=np.number).columns.tolist()
-categorical_cols = dataset.select_dtypes(include='object').columns.tolist()
+numerical_columns = dataset.select_dtypes(include=np.number).columns.tolist()
+categorical_columns = dataset.select_dtypes(include='object').columns.tolist()
 
-# Remove the target column from numerical columns
-if target_col in numerical_cols:
-    numerical_cols.remove(target_col)
+if target_col in numerical_columns:
+    numerical_columns.remove(target_col)
 
-# Group the numerical columns by 2
-grouped_cols = [numerical_cols[i:i + 2] for i in range(0, len(numerical_cols), 2)]
+num_pairs = len(numerical_columns) // 4
+num_rows = int(np.ceil(num_pairs / 4))
 
-# Determine the number of rows and columns for the subplots
-nrows = (len(grouped_cols) + 1) // 3 + ((len(grouped_cols) + 1) % 3 > 0)
-ncols = 3
+# Adjust the figsize to fit on a 13.3-inch (2560 × 1600) screen
+fig, axes = plt.subplots(num_rows, 4, figsize=(15, 2 * num_rows))
 
-# Create the subplots
-fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, nrows * 4), sharey=False)
-axes = axes.ravel()
+for i, (ax, pair) in enumerate(zip(axes.flatten(), range(0, len(numerical_columns), 4))):
+    col1 = numerical_columns[pair]
+    col2 = numerical_columns[pair + 1]
+    col3 = numerical_columns[pair + 2]
+    col4 = numerical_columns[pair + 3]
 
-for i, group in enumerate(grouped_cols):
-    ax = axes[i]
-    for j, col in enumerate(group):
-        dataset[col].value_counts(normalize=True).sort_index().plot(ax=ax, label=col)
+    sns.histplot(dataset, x=col1, color="blue", alpha=0.5, ax=ax, kde=True, label=col1)
+    sns.histplot(dataset, x=col2, color="orange", alpha=0.5, ax=ax, kde=True, label=col2)
+    sns.histplot(dataset, x=col3, color="green", alpha=0.5, ax=ax, kde=True, label=col3)
+    sns.histplot(dataset, x=col4, color="red", alpha=0.5, ax=ax, kde=True, label=col4)
 
-    # Set the y-axis range based on the columns
-    min_value = min(dataset[group].min())
-    max_value = max(dataset[group].max())
-    ax.set_ylim(min_value, max_value)
-
-    ax.set_title(f'Histogram of Columns Group {i + 1}')
-    ax.set_xlabel('Frequency')
-    ax.set_ylabel('Value')
+    ax.set_title(f'Distribution of {col1}, {col2}, {col3}, and {col4}')
     ax.legend()
 
-# Plot the target variable in the last subplot
-ax = axes[-1]
-dataset[target_col].value_counts(normalize=True).sort_index().plot(ax=ax, label=target_col)
-ax.set_title('Histogram of Target Variable')
-ax.set_xlabel('Frequency')
-ax.set_ylabel('Value')
-ax.legend()
-
-# Remove empty subplots if there's an odd number of groups
-if (len(grouped_cols) + 1) % ncols != 0:
-    for i in range((len(grouped_cols) + 1) % ncols, ncols):
-        fig.delaxes(axes[-i])
+# Remove unused subplots
+for j in range(i + 1, num_rows * 4):
+    fig.delaxes(axes.flatten()[j])
 
 plt.tight_layout()
 plt.show()
